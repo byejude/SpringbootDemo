318bd8af97f1117eabdc4fc1833e5596ab055378
byejude4RP <657401262@qq.com>
2017-11-11 23:42:34 +0800

    init

---
+++ src/test/java/com/tulip/girlTest/GirlTestApplicationTests.java
@@ -0,0 +1,18 @@
+package com.tulip.girlTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GirlTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
